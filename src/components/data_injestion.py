fff530d1ed495ee39cbe65e534bf148129d08cd7
mahigithub2080 <mahipatil1593@gmail.com>
2023-08-26 00:54:16 +0530

    injestion.py

--- src/components/data_injestion.py
+++ src/components/data_injestion.py
@@ -1,55 +1,61 @@
 import os
 import sys
 from src.exception import CustomException
-from src.logger import logging
-import pandas as pd
+from src.logger import logging 
 
+import pandas as pd
 from sklearn.model_selection import train_test_split
+
 from dataclasses import dataclass
 
-from src.components.data_transformation import DataTransformation
-from src.components.data_transformation import DataTransformationConfig
-
 @dataclass
-class DataIngestionConfig:
-    train_data_path: str=os.path.join('artifacts',"train.csv")
-    test_data_path: str=os.path.join('artifacts',"test.csv")
-    raw_data_path: str=os.path.join('artifacts',"data.csv")
+class DataInjestionConfig:
+    train_data_path : str=os.path.join('artifacts','train.csv')
+    test_data_path : str=os.path.join('artifacts','test.csv')
+    raw_data_path : str=os.path.join('artifacts','data.csv')
+    # created path for train/test/raw data
 
-class DataIngestion:
+class DataInjestion:
     def __init__(self):
-        self.ingestion_config=DataIngestionConfig()
+        self.injestion_config=DataInjestionConfig() 
+        # assigning all the paths to the injestion_config object / Creating object of DataInjestionConfig
 
-    def initiate_data_ingestion(self):
-        logging.info("Entered the data ingestion method or component")
+    def initiate_data_injestion(self):
+        logging.info("Entered in to injestion method....!")
         try:
-            df=pd.read_csv('notebook/data/stud.csv')
-            logging.info('Read the dataset as dataframe')
+            df=pd.read_csv("notebook\data\stud.csv") # reading file 
+            logging.info("read the dataset as DataFrame.....!")
 
-            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
+            os.makedirs(os.path.dirname(self.injestion_config.train_data_path),exist_ok=True)  
+            # creating the folders with the help of train data path
+            # exist_ok --- if folder is already there.it will keep that folder
 
-            df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
+            df.to_csv(self.injestion_config.raw_data_path,index=False,header=True)
+            # saving the raw data to specified path..
 
-            logging.info("Train test split initiated")
+            logging.info("train_test_split initiated..!")
             train_set,test_set=train_test_split(df,test_size=0.2,random_state=42)
+            train_set.to_csv(self.injestion_config.train_data_path,index=False,header=True)
+            test_set.to_csv(self.injestion_config.test_data_path,index=False,header=True)
+            # saving the train_set,test_set  data to specified path..
 
-            train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
-
-            test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
-
-            logging.info("Ingestion of the data is completed")
+            logging.info("DataInjestion data completed....!")
 
             return(
-                self.ingestion_config.train_data_path,
-                self.ingestion_config.test_data_path
-
+                self.injestion_config.train_data_path,
+                self.injestion_config.test_data_path
             )
+
         except Exception as e:
             raise CustomException(e,sys)
         
 if __name__=="__main__":
-    obj=DataIngestion()
-    train_data,test_data=obj.initiate_data_ingestion()
+    obj=DataInjestion()
+    obj.initiate_data_injestion()
+    
+    
 
-    data_transformation=DataTransformation()
-    data_transformation.initiate_data_transformation(train_data,test_data)
+
+
+
+
