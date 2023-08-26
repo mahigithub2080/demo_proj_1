e5804c093ba4d5b653a543ec4f84a0154ecf9862
mahigithub2080 <mahipatil1593@gmail.com>
2023-08-26 16:06:31 +0530

    transformation.py

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -1,124 +1,77 @@
+## purpose of data_transformation  is to do feature eng, data cleaning, convert cat to neumeric..etc
+import numpy as np
+import pandas as pd
+
+import os
 import sys
 from dataclasses import dataclass
 
-import numpy as np 
-import pandas as pd
-from sklearn.compose import ColumnTransformer
-from sklearn.impute import SimpleImputer
-from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import OneHotEncoder,StandardScaler
+from sklearn.compose import ColumnTransformer # used to create pipeline
+from sklearn.impute import SimpleImputer # handling missing data
+from sklearn.pipeline import Pipeline # create pipeling
+from sklearn.preprocessing import OneHotEncoder,StandardScaler # for transformation
+
 
 from src.exception import CustomException
 from src.logger import logging
-import os
-
-from src.utils import save_object
 
 @dataclass
-class DataTransformationConfig:
-    preprocessor_obj_file_path=os.path.join('artifacts',"proprocessor.pkl")
+class DatatransformationConfig:
+    preprocessor_obj_filepath=os.path.join("artifacts","preprocessor.pkl") 
+    # creating the path for preprocessor.pkl file...
 
 class DataTransformation:
-    def __init__(self):
-        self.data_transformation_config=DataTransformationConfig()
+    def __init__(self) :
+        self.datat_transformation_config=DatatransformationConfig()
+        # creating obj of DatatransformationConfig class..
 
-    def get_data_transformer_object(self):
-        '''
-        This function is responsible for data trnasformation
-        
-        '''
+    def get_data_transformer_obj(self):
+        """
+         This method responsible to for data transformation...!!!...(missing_values/cat_to_num/transform_scaler)
+        """
         try:
-            numerical_columns = ["writing_score", "reading_score"]
-            categorical_columns = [
-                "gender",
+            numerical_feature=['reading_score','writing_score']
+            catagorical_feature=[
+                'gender',
                 "race_ethnicity",
                 "parental_level_of_education",
                 "lunch",
-                "test_preparation_course",
+                "test_preparation_course"
             ]
+            # numerical and catagorical features are seperated
 
-            num_pipeline= Pipeline(
+            num_pipeline=Pipeline(
                 steps=[
-                ("imputer",SimpleImputer(strategy="median")),
-                ("scaler",StandardScaler())
-
-                ]
-            )
-
+                    ("imputer",SimpleImputer(strategy="median")),
+                    ("Scalar", StandardScaler())
+                    ]
+            ) 
+            # creating pipeline for numerical feature
             cat_pipeline=Pipeline(
-
                 steps=[
-                ("imputer",SimpleImputer(strategy="most_frequent")),
-                ("one_hot_encoder",OneHotEncoder()),
-                ("scaler",StandardScaler(with_mean=False))
+                    ("imputer",SimpleImputer(strategy="most_frequent")),
+                    ("oneHotEncoder",OneHotEncoder()),
+                    ("scalar",StandardScaler())
                 ]
-
             )
-
-            logging.info(f"Categorical columns: {categorical_columns}")
-            logging.info(f"Numerical columns: {numerical_columns}")
+            # creating pipeline for catagorical feature
+            logging.info(f"num_pipeline created... and are numerical_fetures".format(numerical_feature))
+            logging.info(f"cat_pipeline created... and are catagorical_feature".format(catagorical_feature))
 
             preprocessor=ColumnTransformer(
                 [
-                ("num_pipeline",num_pipeline,numerical_columns),
-                ("cat_pipelines",cat_pipeline,categorical_columns)
-
+                    ("num_pipeline",num_pipeline,numerical_feature)
+                    ("cat_pipeline",cat_pipeline,catagorical_feature)
                 ]
-
-
-            )
+            ) 
+            #combining num_pipeline and cat_pipeline in to one "preprocessor" object
 
             return preprocessor
-        
+
+
         except Exception as e:
             raise CustomException(e,sys)
-        
 
-    def initiate_data_transformation(self,train_path,test_path):
 
-        try:
-            train_df=pd.read_csv(train_path)
-            test_df=pd.read_csv(test_path)
 
-            logging.info("Read train and test data completed")
 
-            logging.info("Obtaining preprocessing object")
-
-            preprocessing_obj=self.get_data_transformer_object()
-
-            target_column_name="math_score"
-
-            input_feature_train_df=train_df.drop(columns=[target_column_name],axis=1)
-            target_feature_train_df=train_df[target_column_name]
-
-            input_feature_test_df=test_df.drop(columns=[target_column_name],axis=1)
-            target_feature_test_df=test_df[target_column_name]
-
-            logging.info(
-                f"Applying preprocessing object on training dataframe and testing dataframe."
-            )
-
-            input_feature_train_arr=preprocessing_obj.fit_transform(input_feature_train_df)
-            input_feature_test_arr=preprocessing_obj.transform(input_feature_test_df)
-
-            train_arr = np.c_[
-                input_feature_train_arr, np.array(target_feature_train_df)
-            ]
-            test_arr = np.c_[input_feature_test_arr, np.array(target_feature_test_df)]
-
-            logging.info(f"Saved preprocessing object.")
-
-            save_object(
-
-                file_path=self.data_transformation_config.preprocessor_obj_file_path,
-                obj=preprocessing_obj
-
-            )
-
-            return (
-                train_arr,
-                test_arr,
-                self.data_transformation_config.preprocessor_obj_file_path,
-            )
-        except Exception as e:
-            raise CustomException(e,sys)
